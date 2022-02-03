8edb464890312ad9f05c0414abdaa5f95ff530e9
RayanKhyare <71662425+RayanKhyare@users.noreply.github.com>
2022-02-03 11:15:03 +0100

    Made whole website beside details

--- DeepfakeSimulator/script.js
+++ DeepfakeSimulator/script.js
@@ -5,25 +5,128 @@ window.onload = function () {
     const container = document.getElementById("leftside")
     const rightside = document.getElementById("rightside")
 
-    const einsteintext = document.getElementById("einsteintext")
+    const einsteintext = document.getElementById("alberttext")
     const baptistetext = document.getElementById("baptistetext")
     const olivetext = document.getElementById("olivetext")
 
-    const nlButtonBaptiste = document.getElementById("buttonNLBaptiste")
-    const frButtonBaptiste = document.getElementById("buttonFRBaptiste")
-    const enButtonBaptiste = document.getElementById("buttonENBaptiste")
+    einsteintext.addEventListener("click", function (e) {
+        e.preventDefault();
+
+
+        let htmlString = ` <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+        <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+            src="./ressources/einsteinStandby.mp4" muted autoplay loop>
+        </video>`
+
+        container.innerHTML = htmlString
+
+        let htmlString2 = `<div class="buttons">
+        <button id='buttonNL' class="button" type="buttonNL">NL</button>
+        <button id='buttonFR' class="button" type="buttonFR">FR</button>
+        <button id='buttonEN' class="button" type="buttonEN">EN</button>
+    </div>`
+
+        rightside.innerHTML = htmlString2
+
+        EinsteinButtonsFinder()
+    })
+
+
+
+    function EinsteinButtonsFinder() {
+        const nlButton = document.getElementById("buttonNL")
+        const frButton = document.getElementById("buttonFR")
+        const enButton = document.getElementById("buttonEN")
+
+        nlButton.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let htmlString = `<img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+            src="./ressources/einsteinNL.mp4" autoplay>
+        </video>`
+
+            container.innerHTML = htmlString
+            const video = document.getElementById("deepfake")
+            video.volume = 0.5;
+
+            video.onended = function () {
+
+                let htmlString = `
+                <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+                 <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+                        src="./ressources/einsteinStandby.mp4" muted autoplay loop>
+                    </video>`
+
+                container.innerHTML = htmlString
+            }
+        })
+
+        enButton.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let htmlString = `
+            <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+            src="./ressources/einsteinEN.mp4" autoplay>
+        </video>`
+
+            container.innerHTML = htmlString
+            const video = document.getElementById("deepfake")
+            video.volume = 0.5;
+
+            video.onended = function () {
+
+                let htmlString = ` 
+                <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+                <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+                        src="./ressources/einsteinStandby.mp4" muted autoplay loop>
+                    </video>`
+
+                container.innerHTML = htmlString
+            }
+        })
+
+        frButton.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            alert(`La version française n'est pas encore disponible. Désolé pour le désagrément !`)
+        })
+
+    }
+
+    olivetext.addEventListener("click", function (e) {
+        e.preventDefault();
+
+
+
+        let htmlString = ` <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+        <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+            src="./ressources/oliveStandby.mp4" muted autoplay loop>
+        </video>`
+
+        container.innerHTML = htmlString
+
+        let htmlString2 = `<div class="buttons buttonsOlive">
+        <button id='buttonNLOlive' class="button" type="buttonNL">NL</button>
+        <button id='buttonFROlive' class="button" type="buttonFR">FR</button>
+        <button id='buttonENOlive' class="button" type="buttonEN">EN</button>
+    </div>`
+
+        rightside.innerHTML = htmlString2
+
+        OliveButtonsFinder()
+    })
+
 
     baptistetext.addEventListener("click", function (e) {
         e.preventDefault();
 
-        if (document.querySelector('text') !== null) {
-            document.querySelector('text').classList.remove('active');
-        }
-        e.target.className = "active";
 
-        baptistetext.classList.add("active");
 
-        let htmlString = `<video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
+        let htmlString = `
+        <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+        <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
         src="./ressources/baptisteStandby.mp4" muted autoplay loop>
     </video>`
 
@@ -40,6 +143,69 @@ window.onload = function () {
         BaptisteButtonsFinder()
     })
 
+    function OliveButtonsFinder() {
+        const nlButtonOLive = document.getElementById("buttonNLOlive")
+        const frButtonOlive = document.getElementById("buttonFROlive")
+        const enButtonOlive = document.getElementById("buttonENOlive")
+
+        nlButtonOLive.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let htmlString = `<img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+            src="./ressources/oliveNL.mp4" autoplay>
+        </video>`
+
+            container.innerHTML = htmlString
+            const video = document.getElementById("deepfake")
+            video.volume = 0.5;
+
+            video.onended = function () {
+
+                let htmlString = `
+                <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+                 <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+                        src="./ressources/oliveStandby.mp4" muted autoplay loop>
+                    </video>`
+
+                container.innerHTML = htmlString
+            }
+        })
+
+        enButtonOlive.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let htmlString = `
+            <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+            src="./ressources/oliveEN.mp4" autoplay>
+        </video>`
+
+            container.innerHTML = htmlString
+            const video = document.getElementById("deepfake")
+            video.volume = 0.5;
+
+            video.onended = function () {
+
+                let htmlString = ` 
+                <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+                <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+                        src="./ressources/oliveStandby.mp4" muted autoplay loop>
+                    </video>`
+
+                container.innerHTML = htmlString
+            }
+        })
+
+        frButtonOlive.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            alert(`La version française n'est pas encore disponible. Désolé pour le désagrément !`)
+        })
+
+    }
+
+
     function BaptisteButtonsFinder() {
         const nlButtonBaptiste = document.getElementById("buttonNLBaptiste")
         const frButtonBaptiste = document.getElementById("buttonFRBaptiste")
@@ -48,7 +214,9 @@ window.onload = function () {
         nlButtonBaptiste.addEventListener("click", function (e) {
             e.preventDefault();
 
-            let htmlString = `<video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
+            let htmlString = `
+            <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
             src="./ressources/baptisteNL.mp4" autoplay>
         </video>`
 
@@ -58,12 +226,45 @@ window.onload = function () {
 
             video.onended = function () {
 
-                let htmlString = ` <video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
+                let htmlString = `
+                <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+                <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
                         src="./ressources/baptisteStandby.mp4" muted autoplay loop>
                     </video>`
 
                 container.innerHTML = htmlString
             }
+        })
+
+        enButtonBaptiste.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let htmlString = `
+            <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+            src="./ressources/baptisteEN.mp4" autoplay>
+        </video>`
+
+            container.innerHTML = htmlString
+            const video = document.getElementById("deepfake")
+            video.volume = 0.5;
+
+            video.onended = function () {
+
+                let htmlString = ` 
+                <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+                <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
+                        src="./ressources/baptisteStandby.mp4" muted autoplay loop>
+                    </video>`
+
+                container.innerHTML = htmlString
+            }
+        })
+
+        frButtonBaptiste.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            alert(`La version française n'est pas encore disponible. Désolé pour le désagrément !`)
         })
 
     }
@@ -77,7 +278,9 @@ window.onload = function () {
     nlButton.addEventListener("click", function (e) {
         e.preventDefault();
 
-        let htmlString = `<video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
+        let htmlString = `
+        <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+        <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
         src="./ressources/einsteinNL.mp4" autoplay>
     </video>`
 
@@ -87,7 +290,9 @@ window.onload = function () {
 
         video.onended = function () {
 
-            let htmlString = ` <video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
+            let htmlString = ` 
+            <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
                     src="./ressources/einsteinStandby.mp4" muted autoplay loop>
                 </video>`
 
@@ -99,17 +304,16 @@ window.onload = function () {
     frButton.addEventListener("click", function (e) {
         e.preventDefault();
 
-        alert('FR Button is clicked')
-        htmlString = `<video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
-        src="./ressources/einsteinFR.mp4" autoplay>
-    </video>`
-        container.innerHTML(htmlString)
+        alert(`La version française n'est pas encore disponible. Désolé pour le désagrément !`)
+
     })
 
     enButton.addEventListener("click", function (e) {
         e.preventDefault();
 
-        let htmlString = `<video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
+        let htmlString = `
+        <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+        <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
         src="./ressources/einsteinEN.mp4" autoplay>
     </video>`
 
@@ -119,7 +323,9 @@ window.onload = function () {
 
         video.onended = function () {
 
-            let htmlString = ` <video id="deepfake" title="Deepfake" width="436" height="650" type="video/mp4"
+            let htmlString = `
+            <img class="mockuplogo" src="./ressources/mockupWebsite.png">
+            <video id="deepfake" title="Deepfake" width="250" height="650" type="video/mp4"
                     src="./ressources/einsteinStandby.mp4" muted autoplay loop>
                 </video>`
 
